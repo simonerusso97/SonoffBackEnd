691b0699b0ccccb63fe7b5d4a52ad3ae61a2c904
Simone Russo <34507615+simonerusso97@users.noreply.github.com>
2021-11-09 13:22:17 +0100

    added saveToken()

--- src/main/java/it/unisalento/sonoffbackend/RestController.java
+++ src/main/java/it/unisalento/sonoffbackend/RestController.java
@@ -2,9 +2,11 @@ package it.unisalento.sonoffbackend;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.squareup.okhttp.FormEncodingBuilder;
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.Response;
@@ -18,7 +20,9 @@ public class RestController {
 
 	@RequestMapping(value = "changeStatusOFF", method = RequestMethod.GET)
 	public ResponseEntity<Boolean> changeStatusOFF() throws Exception {
-		Request request = new Request.Builder().url(host+"changeStatusOFF").build();
+		Request request = new Request.Builder().url(host+"changeStatusOFF")
+				.get()
+				.build();
 		Response response = client.newCall(request).execute();
 		if (response.isSuccessful()) {
 			return new ResponseEntity<>(HttpStatus.OK);
@@ -29,7 +33,9 @@ public class RestController {
 
 	@RequestMapping(value = "changeStatusON", method = RequestMethod.GET)
 	public ResponseEntity<Boolean> changeStatusON() throws Exception {
-		Request request = new Request.Builder().url(host+"changeStatusON").build();
+		Request request = new Request.Builder().url(host+"changeStatusON")
+				.get()
+				.build();
 		Response response = client.newCall(request).execute();
 		if (response.isSuccessful()) {
 			return new ResponseEntity<>(HttpStatus.OK);
@@ -38,11 +44,29 @@ public class RestController {
 
 	}
 
-	//TODO: response come funziona?
-	  @RequestMapping(value="getStatus", method = RequestMethod.GET) public String getStatus() throws Exception{
-		   Request request = new Request.Builder().url(host+"getStatus").build();
+	  @RequestMapping(value="getStatus", method = RequestMethod.GET) 
+	  public String getStatus() throws Exception{
+		   Request request = new Request.Builder().url(host+"getStatus")
+					.get()
+				   .build();
 		   Response response = client.newCall(request).execute();
 		   return response.body().string();
+	  }
+	  
+	  @RequestMapping(value="saveToken", method = RequestMethod.POST) 
+	  public ResponseEntity<String> saveToken(@RequestBody String token) throws Exception{
+		  String tokenValue = token.split("=")[1];
+
+		  
+	        com.squareup.okhttp.RequestBody formBody = new FormEncodingBuilder()
+	                .add("token", tokenValue)
+	                .build();
+		   
+	        Request request = new Request.Builder().url(host+"saveToken")
+	        		.post(formBody)
+				   .build();
+		   Response response = client.newCall(request).execute();
+		return new ResponseEntity<String>(HttpStatus.OK);
 	  }
 	
 }
